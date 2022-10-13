2df9eb08025d4b38e3097f2e9ca45d51510b5fd3
Jonathan Dance <wuputah@users.noreply.github.com>
2022-10-13 14:28:18 -0500

    escape $ to avoid shell expansion

--- files/default/postgres-appliance/scripts/post_init.sh
+++ files/default/postgres-appliance/scripts/post_init.sh
@@ -194,7 +194,7 @@ CREATE EXTENSION IF NOT EXISTS pg_stat_kcache SCHEMA public;
 CREATE EXTENSION IF NOT EXISTS set_user SCHEMA public;
 ALTER EXTENSION set_user UPDATE;
 UPDATE pg_catalog.pg_extension SET extname = 'columnar' WHERE extname = 'citus_columnar';
-UPDATE pg_catalog.pg_proc SET probin = '$libdir/columnar' WHERE probin = '$libdir/citus_columnar';
+UPDATE pg_catalog.pg_proc SET probin = '\$libdir/columnar' WHERE probin = '\$libdir/citus_columnar';
 CREATE EXTENSION IF NOT EXISTS columnar;
 ALTER EXTENSION columnar UPDATE;
 GRANT EXECUTE ON FUNCTION public.set_user(text) TO admin;
