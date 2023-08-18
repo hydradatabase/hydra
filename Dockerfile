345baf7243afce94b27803e465c83c8c80b4e032
Jonathan Dance <wuputah@users.noreply.github.com>
2023-08-18 16:00:37 -0500

    use pgxman yaml files

--- Dockerfile
+++ Dockerfile
@@ -52,7 +52,7 @@ COPY files/postgres/docker-entrypoint-initdb.d /docker-entrypoint-initdb.d/
 ARG POSTGRES_BASE_VERSION
 # Always force rebuild of this layer
 ARG TIMESTAMP=1
-COPY third-party/pgxman /tmp
+COPY third-party/pgxman /tmp/pgxman/
 RUN set -eux; \
     /tmp/pgxman/install.sh ${POSTGRES_BASE_VERSION}; \
     rm -rf /tmp/pgxman
--- third-party/pgxman/install.sh
+++ third-party/pgxman/install.sh
@@ -2,12 +2,15 @@
 
 set -euo pipefail
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 main() {
   install_pgxman
 
   local _version
   for _version in "$@"; do
-    install_extensions "${_version}"
+    echo "Installing extensions for PostgreSQL ${_version}..."
+    pgxman install --file "${SCRIPT_DIR}/pgxman_${_version}.yaml"
   done
 }
 
@@ -20,45 +23,6 @@ install_pgxman () {
   wget -O "/tmp/pgxman_linux_${_arch}.deb" "https://github.com/pgxman/release/releases/latest/download/pgxman_linux_${_arch}.deb"
   apt install -y "/tmp/pgxman_linux_${_arch}.deb"
   pgxman update
-}
-
-install_extensions () {
-  local _pg_version="$1"
-
-  get_extensions $_pg_version || return 1
-  local _extensions=( "${RETVAL[@]}" )
-
-  echo "Installing extensions for PostgreSQL $_pg_version:"
-  for _ext in "${_extensions[@]}"; do
-    echo "- $_ext"
-  done
-
-  local _packages=()
-  for _ext in "${_extensions[@]}"; do
-    _packages+=("$(printf "%s@%s " "$_ext" "$_pg_version")")
-  done
-  printf -v _packages_args '%s ' "${_packages[@]}"
-
-  pgxman install $_packages_args
-}
-
-get_extensions () {
-  local _pg_version="$1"
-
-  local _universal_extensions=(
-    "pgvector=0.4.4"
-  )
-  # each is a space-delimited list
-  local _per_version_extensions=(
-    [13]="pg_hint_plan-13=1.3.8-1"
-    [14]="pg_hint_plan-14=1.4.1-1"
-    [15]="pg_hint_plan-15=1.5.0-1"
-  )
-
-  local _relevant_extensions="${_per_version_extensions[$_pg_version]}"
-
-  # do not quote _relevant_extensions to allow word splitting
-  RETVAL=( "${_universal_extensions[@]}" ${_relevant_extensions} )
 }
 
 get_architecture () {
